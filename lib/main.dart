7aac2d74023041295b723c890f1bb54a332e1b7c
JesusTirado07 <106884781+JesusTirado07@users.noreply.github.com>
2023-02-01 12:24:53 -0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_digilist/screens/carrito.dart';
-import 'package:proyecto_digilist/screens/catalogo.dart';
-import 'package:proyecto_digilist/screens/envio.dart';
-import 'package:proyecto_digilist/screens/favoritos.dart';
-import 'package:proyecto_digilist/screens/historial.dart';
-import 'package:proyecto_digilist/screens/login_screen.dart';
-import 'package:proyecto_digilist/screens/Home.dart';
-import 'package:proyecto_digilist/screens/pago.dart';
-import 'package:proyecto_digilist/screens/perfil.dart';
-import 'package:proyecto_digilist/screens/ticket.dart';
-import 'package:proyecto_digilist/screens/vistaProducto.dart';
+import 'package:flutter_login_ui/screens/login_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,22 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pagina de inicio',
-      /* initialRoute: '/',
-      routes: {
-        '/': (context) =>  Home(),
-      '/login': (context) =>  LoginScreen(),
-      '/carrito': (context) =>  Carrito_Compra(),
-      '/catalogo': (context) =>  Catalogo(),
-      '/favoritos': (context) =>  Favoritos(),
-      '/historial': (context) =>  Historial(),
-      '/pago': (context) =>  Pago(),
-      '/perfil': (context) =>  Perfil(),
-      '/ticket': (context) =>  Ticket(),
-      '/vista_Producto': (context) =>  Vista_Producto(),
-      '/envio': (context) =>  Envio(),
-      }, */
       debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
   }
 }
+
